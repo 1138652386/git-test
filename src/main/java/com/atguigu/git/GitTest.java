326ff291be374a32f8d0006a36abd82d5ad60853
Jianxin <1138652386@qq.com>
2022-09-26 21:49:27 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Jianxin Pei
+ * @create 2022-09-26 21:46
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello  git");
+    }
+}
